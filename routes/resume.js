09b47dc90f0e6b34a8269218678e6abff49dd951
Transparent_H <18098581870@163.com>
2024-02-23 16:45:26 +0800

    修改简历路径的写法

--- routes/resume.js
+++ routes/resume.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var router = express.Router()
 const fs = require('fs')
+const path = require('path')
 
 router.get('/', function (req, res, next) {
   try {
-    let resumeInfo = fs.readFileSync('./public/markdown/resume.md')
+    let resumeInfo = fs.readFileSync(path.join(__dirname, '../public/markdown/resume.md'))
     res.send(resumeInfo)
   } catch (err) {
     console.error(err)
